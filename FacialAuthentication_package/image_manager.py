7efbff2665996498190642b93071b87816e618da
Roberto Franchini <roberto.franchini@student.h-farm.com>
2020-11-30 16:24:32 +0100

    added code for image compressor

---
+++ FacialAuthentication_package/image_manager.py
@@ -0,0 +1,18 @@
+import os
+from PIL import Image
+
+
+def image_weight(image):
+    input_image = 'check.jpg'
+    dimension_kb = os.stat(image).st_size
+    dimension_mb = dimension_kb/(1024*1024)
+    return dimension_mb
+
+
+def comprime_image(input_image):
+    size = image_weight(input_image)
+    while size > 3.4:
+        picture = Image.open(input_image)
+        picture.save(input_image,optimize=True,quality=30)
+        size = image_weight(input_image)        
+    return input_image
